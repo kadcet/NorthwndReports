26caf0a6d810b3f779e91ffa394292daa6e05225
Kadir Çetin <kadirrcetin@gmail.com>
2023-07-21 15:10:41 +0300

    210723-1

--- WinFormUI/Form1.Designer.cs
+++ WinFormUI/Form1.Designer.cs
@@ -1,61 +1,74 @@
 ﻿namespace WinFormUI
 {
-    partial class Form1
-    {
-        /// <summary>
-        ///  Required designer variable.
-        /// </summary>
-        private System.ComponentModel.IContainer components = null;
+	partial class Form1
+	{
+		/// <summary>
+		///  Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        ///  Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing && (components != null))
-            {
-                components.Dispose();
-            }
-            base.Dispose(disposing);
-        }
+		/// <summary>
+		///  Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
 
-        #region Windows Form Designer generated code
+		#region Windows Form Designer generated code
 
-        /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
-        /// </summary>
-        private void InitializeComponent()
-        {
-            grdReport = new DataGridView();
-            ((System.ComponentModel.ISupportInitialize)grdReport).BeginInit();
-            SuspendLayout();
-            // 
-            // grdReport
-            // 
-            grdReport.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            grdReport.Location = new Point(105, 87);
-            grdReport.Name = "grdReport";
-            grdReport.RowTemplate.Height = 25;
-            grdReport.Size = new Size(602, 293);
-            grdReport.TabIndex = 0;
-            // 
-            // Form1
-            // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
-            AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(grdReport);
-            Name = "Form1";
-            Text = "Form1";
-            Load += Form1_Load;
-            ((System.ComponentModel.ISupportInitialize)grdReport).EndInit();
-            ResumeLayout(false);
-        }
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			grdReport = new DataGridView();
+			button1 = new Button();
+			((System.ComponentModel.ISupportInitialize)grdReport).BeginInit();
+			SuspendLayout();
+			// 
+			// grdReport
+			// 
+			grdReport.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			grdReport.Location = new Point(105, 87);
+			grdReport.Name = "grdReport";
+			grdReport.RowTemplate.Height = 25;
+			grdReport.Size = new Size(602, 293);
+			grdReport.TabIndex = 0;
+			// 
+			// button1
+			// 
+			button1.Location = new Point(128, 25);
+			button1.Name = "button1";
+			button1.Size = new Size(274, 23);
+			button1.TabIndex = 1;
+			button1.Text = "Hangi Kategoriden Kaç Ürün Var";
+			button1.UseVisualStyleBackColor = true;
+			button1.Click += button1_Click;
+			// 
+			// Form1
+			// 
+			AutoScaleDimensions = new SizeF(7F, 15F);
+			AutoScaleMode = AutoScaleMode.Font;
+			ClientSize = new Size(800, 450);
+			Controls.Add(button1);
+			Controls.Add(grdReport);
+			Name = "Form1";
+			Text = "Form1";
+			Load += Form1_Load;
+			((System.ComponentModel.ISupportInitialize)grdReport).EndInit();
+			ResumeLayout(false);
+		}
 
-        #endregion
+		#endregion
 
-        private DataGridView grdReport;
-    }
+		private DataGridView grdReport;
+		private Button button1;
+	}
 }
--- WinFormUI/Form1.cs
+++ WinFormUI/Form1.cs
@@ -3,35 +3,61 @@ using WinFormUI.Data;
 
 namespace WinFormUI
 {
-    public partial class Form1 : Form
-    {
-        public Form1()
-        {
-            InitializeComponent();
-        }
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            //Hangi kategoriden kaç TL lik satış yapılmış
-            NorthwndDBContext context = new NorthwndDBContext();
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			//Hangi kategoriden kaç TL lik satış yapılmış
+			NorthwndDBContext context = new NorthwndDBContext();
 
-            var report = (from product in context.Set<Product>()
-                          join category in context.Set<Category>()
-                                 on product.CategoryID equals category.CategoryID
-                          join orderdetail in context.Set<OrderDetail>() 
-                          on product.ProductID equals orderdetail.ProductID
-                          group product by product.ProductName into g
-                          // group category by category.CategoryName into g
-                          select new 
-                          {
-                              
-                              g.Key, Count=g.Count(),
-
-                          }) ;
-
-            grdReport.DataSource = report.ToList();
+			var report = (from product in context.Products
+						  join category in context.Categories
+								 on product.CategoryID equals category.CategoryID
+						  join orderdetail in context.OrderDetails
+						  on product.ProductID equals orderdetail.ProductID
+						  //group product by new {product.ProductName,orderdetail.Quantity} into grp
+						  group product by new { category, orderdetail } into grp
+						  select new
+						  {
+							 
+							 ProductName
+							  
+						  });
 
 
-        }
-    }
+			grdReport.DataSource = report.ToList();
+
+
+		}
+
+		
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			NorthwndDBContext context = new NorthwndDBContext();
+
+			var report2 = (from product in context.Set<Product>()
+						   join category in context.Set<Category>()
+								  on product.CategoryID equals category.CategoryID
+						   join orderdetail in context.Set<OrderDetail>()
+						   on product.ProductID equals orderdetail.ProductID
+						   // group product by product.ProductName into g
+						   group category by category.CategoryName into g
+						   select new
+						   {
+
+							   g.Key,
+							   Count = g.Count(),
+
+
+						   });
+			grdReport.DataSource = report2.ToList();
+			
+		}
+	}
 }
