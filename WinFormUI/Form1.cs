3f295fd5a90883910dc68696b5738f9767e7830b
Kadir Çetin <kadirrcetin@gmail.com>
2023-08-13 15:21:04 +0300

    1308-1

--- WinFormUI/Form1.cs
+++ WinFormUI/Form1.cs
@@ -15,19 +15,38 @@ namespace WinFormUI
 			//Hangi kategoriden kaç TL lik satış yapılmış
 			NorthwndDBContext context = new NorthwndDBContext();
 
-			var report = (from product in context.Products
-						  join category in context.Categories
-								 on product.CategoryID equals category.CategoryID
-						  join orderdetail in context.OrderDetails
-						  on product.ProductID equals orderdetail.ProductID
-						  //group product by new {product.ProductName,orderdetail.Quantity} into grp
-						  group product by new { category, orderdetail } into grp
-						  select new
-						  {
+			//var report = (from product in context.Products
+			//			  join category in context.Categories
+			//					 on product.CategoryID equals category.CategoryID
+			//			  join orderdetail in context.OrderDetails
+			//			  on product.ProductID equals orderdetail.ProductID
+			//			  //group product by new {product.ProductName,orderdetail.Quantity} into grp
+			//			  group product by new { category, orderdetail } into grp
+			//			  select new
+			//			  {
 							 
-							 ProductName
+			//				cc= grp.Sum(x=> x),
 							  
-						  });
+			//			  });
+
+			var report=context.Products
+			 .Join(context.OrderDetails,
+			 product=>product.ProductID,
+			 orderDetail=>orderDetail.ProductID,
+			 (product, orderDetail) => new
+			 {
+				 Category=product.Category.CategoryName,
+				 ProductName = product.ProductName,
+				 Price=orderDetail.UnitPrice * orderDetail.Quantity,
+			 })
+			 .GroupBy(result=>result.ProductName)
+			 .Select(group => new
+			 {
+				 
+				 ProductName= group.Key,
+				 Price=group.Sum(result=>result.Price)
+			 }
+			 ).ToList();
 
 
 			grdReport.DataSource = report.ToList();
@@ -40,23 +59,27 @@ namespace WinFormUI
 		private void button1_Click(object sender, EventArgs e)
 		{
 			NorthwndDBContext context = new NorthwndDBContext();
+            var report2 = context.Categories
+			 .GroupJoin(context.Products,
+			 category => category.CategoryID,
+			 product => product.CategoryID,
+			 (category, product) => new 
+             {
+                 CategoryName = category.CategoryName,
+                 Product = product
+             })
 
-			var report2 = (from product in context.Set<Product>()
-						   join category in context.Set<Category>()
-								  on product.CategoryID equals category.CategoryID
-						   join orderdetail in context.Set<OrderDetail>()
-						   on product.ProductID equals orderdetail.ProductID
-						   // group product by product.ProductName into g
-						   group category by category.CategoryName into g
-						   select new
-						   {
+			.SelectMany(result=>result.Product.DefaultIfEmpty(),
+			(result, product) => new
+			{
+				result.CategoryName,
+				
+				ProductName=product !=null ? product.ProductName:"No Product",
+				TotalPrice=context.OrderDetails.Sum(orderProduct=>orderProduct.UnitPrice * orderProduct.Quantity)
+			}).ToList();
+			
 
-							   g.Key,
-							   Count = g.Count(),
-
-
-						   });
-			grdReport.DataSource = report2.ToList();
+            grdReport.DataSource = report2.ToList();
 			
 		}
 	}
